287003084505bf670631bdf3e7547f05c7b8462d
soniyav20 <soniyav2003@gmail.com>
2023-09-20 17:39:52 +0530

    Splash Screen Working

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:holo_zoo/views/onboarding_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -13,7 +12,6 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
-  late Animation<double> _translateAnimation;
 
   @override
   void initState() {
@@ -22,21 +20,15 @@ class _SplashScreenState extends State<SplashScreen>
       vsync: this,
       duration: const Duration(milliseconds: 2000),
     );
-    _translateAnimation = Tween<double>(begin: 0, end: 1).animate(
-      CurvedAnimation(
-        parent: _animationController,
-        curve: Curves.easeIn,
-      ),
-    );
     Timer(
       const Duration(seconds: 2),
       () async {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (_) => const OnBoarding(),
-          ),
-        );
+        // Navigator.pushReplacement(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: (_) => const OnBoarding(),
+        //   ),
+        // );
       },
     );
     _animationController.forward();
@@ -50,27 +42,23 @@ class _SplashScreenState extends State<SplashScreen>
 
   @override
   Widget build(BuildContext context) {
-    final Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          Center(
-            child: FadeTransition(
-                opacity: _translateAnimation,
-                child: ShaderMask(
-                  shaderCallback: (bounds) => LinearGradient(
-                    colors: [Color(0xff438875), Color(0xff99F2C8)],
-                  ).createShader(bounds),
-                  child: Text(
-                    'HoloZoo',
-                    style: TextStyle(
-                      fontSize: 40.0,
-                    ),
-                  ),
-                )),
+      body: Center(
+        child: ShaderMask(
+          shaderCallback: (bounds) => LinearGradient(
+            colors: [Color(0xff438875), Color(0xff99F2C8)],
+          ).createShader(bounds),
+          child: Text(
+            'HoloZoo',
+            style: TextStyle(
+              fontSize: 40.0,
+              letterSpacing: 1,
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
