b36f124b478a1a328b72bbe5bbc3db8d0f612db0
soniyav20 <soniyav2003@gmail.com>
2023-09-20 17:27:45 +0530

    Splash Screen Added

---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:holo_zoo/views/onboarding_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _translateAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 2000),
+    );
+    _translateAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeIn,
+      ),
+    );
+    Timer(
+      const Duration(seconds: 2),
+      () async {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (_) => const OnBoarding(),
+          ),
+        );
+      },
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          Center(
+            child: FadeTransition(
+                opacity: _translateAnimation,
+                child: ShaderMask(
+                  shaderCallback: (bounds) => LinearGradient(
+                    colors: [Color(0xff438875), Color(0xff99F2C8)],
+                  ).createShader(bounds),
+                  child: Text(
+                    'HoloZoo',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                    ),
+                  ),
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+}
