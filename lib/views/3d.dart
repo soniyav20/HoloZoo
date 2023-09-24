91205fa48f6cf46d0dd1713f3affa726ce5acc98
soniyav20 <soniyav2003@gmail.com>
2023-09-24 07:57:40 +0530

    Iframe Widget Added To View 3D Model

--- lib/views/3d.dart
+++ lib/views/3d.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cube/flutter_cube.dart';
-import 'package:model_viewer_plus/model_viewer_plus.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ThreeD extends StatefulWidget {
   @override
@@ -8,25 +7,43 @@ class ThreeD extends StatefulWidget {
 }
 
 class _ThreeDState extends State<ThreeD> {
-  late Object astro;
   @override
   void initState() {
-    astro = Object(fileName: "assets/sloth.glb");
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    WebViewController controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.dataFromString(
+          '<html><body><iframe id="view360Iframe" width="100%" height="100%" scrolling="0" src="https://sketchfab.com/models/8d913bda48f84217902e6829982c494f/embed?ui_infos=0&amp;ui_watermark=0&amp;ui_help=0&amp;ui_settings=0&amp;ui_inspector=0&amp;ui_annotations=0&amp;ui_stop=0&amp;ui_vr=0&amp;preload=1&amp;autostart=1&amp;ui_hint=2&amp;autospin=0.2"></iframe></body></html>',
+          mimeType: 'text/html'));
     return Scaffold(
-      appBar: AppBar(title: const Text('Model Viewer')),
-      body: ModelViewer(
-        backgroundColor: Colors.teal,
-        src: 'assets/sloth.glb',
-        alt: "A 3D model of an table soccer",
-        autoPlay: true,
-        autoRotate: true,
-        cameraControls: true,
-      ),
-    );
+        extendBodyBehindAppBar: true,
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: WebViewWidget(controller: controller)),
+        ));
   }
 }
